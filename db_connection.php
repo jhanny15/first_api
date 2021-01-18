693d5ecab27f93bd6221789955636d84e0cdb5ac
jhanny15 <jhannycortes@gmail.com>
2021-01-18 09:40:19 +0100

    Aggiunto la connessione al DB

---
+++ db_connection.php
@@ -0,0 +1,14 @@
+<?php
+
+    // Create connection
+    $conn = new mysqli(
+        'mydatabase',   //host
+        'root',         //username
+        'pass',         //password
+        'blog_db');     //database
+
+
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
