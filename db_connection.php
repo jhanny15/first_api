53840502f6de0e55043de6edfca21e8eaa7fe885
jhanny15 <jhannycortes@gmail.com>
2021-01-18 10:29:45 +0100

    Pulito db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -13,14 +13,3 @@
       die("Connection failed: " . $conn->connect_error);
     }
 
-     //Query
-    $query = "SELECT comment_ID, comment_name FROM Comment WHERE post_ID = '2'";
-    $result = $conn->query($query);
-
-    
-    //Fetch Query
-    if($result){
-      while($row = $result->fetch_array(MYSQLI_NUM)){
-        echo json_encode($row);
-      }
-   }
